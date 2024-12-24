09067a6a5e4f8f6315e86b0de576ea129144af8a
AlexSulll <hlebusheck.tv@gmail.com>
2024-12-24 17:08:30 +0300

    awdawddawaw

--- test.asm
+++ test.asm
@@ -2,9 +2,9 @@
 .386
 .stack      100h
 .data
-    A           db      ?
-    B           db      ?
-    C           db      ?
+    A           db      52d
+    B           db      -52d
+    C           db      52d
     D           dd      ?
 .code
 Start:
@@ -17,8 +17,11 @@ Start:
     push    offset C
     
     call    schet
+    mov     [D],eax
     
-    jmp     Exit
+Exit:
+    mov     ah,4Ch
+    int     21h
 schet proc near
     mov     si,[esp+2]
     mov     ch,[si]
@@ -26,71 +29,32 @@ schet proc near
     mov     bh,[si]
     mov     si,[esp+6]
     mov     bl,[si]
-Answer:
-    movsx   eax,bh
-    mov     ebp,eax
-    sal     ebp,2
-    sal     eax,1
+    movsx   eax,bh ;eax = b
+    mov     ebp,eax ;ebp = b
+    sal     ebp,2 ; ebp = 4b
+    sal     eax,1 ;eax = 2b
+    add     ebp,eax ;ebp = 6b
+    movsx   eax,bl ; eax = a
+    imul    ebp;eax = 6ab
+    movsx   eax,ch
     add     ebp,eax
-    movsx   eax,bl
-    imul    ebp
-    or      edx,edx
-    jz      positive_6ab
-    cmp     edx, 0FFFFFFFFh
-    jz      negative_6ab
-positive_6ab:
-    mov     esi,eax 
-    movsx   eax,ch
-    or      eax,eax
-    js      negative_c1
-    add     esi,eax
-    jz      vihod
-    jmp     chislitel
-negative_6ab:    
-    mov     esi,eax
-    movsx   eax,ch
-    or      eax,eax
-    js      negative_c2
-    add     esi,eax
-    jz      vihod
-    jmp     chislitel
-negative_c1:
-    neg     eax
-    sub     esi,eax
-    jz      vihod
-    jmp     chislitel
-negative_c2:
-    neg     eax
-    sub     esi,eax
-    jz      vihod
-chislitel:
-    movsx   eax,ch
-    mov     ebp,eax
-    movsx   eax,bh
-    mov     edi,eax
-    mov     edx,eax
+    jz      Exit
+    mov     esi,ebp
+    movsx   eax,ch ;eax = c
+    mov     ebp,eax ;ebp = c
+    movsx   eax,bh ; eax = b
+    mov     edi,eax ; edi = b
+    mov     edx,eax ; edx = b
     sal     edi,5
     sal     edx,2
-    sub     edi,edx
-    imul    eax
-    imul    edi
+    sub     edi,edx ; edi = 28b
+    imul    eax ; eax = b^2
+    imul    edi ;eax = 28 b^3
     sub     ebp,19
-    jns     positive_c
-    jz      division
-negative_c:
-    neg     ebp
-    sub     eax,ebp
-    jmp     division
-positive_c:
-    add     eax,ebp
-division:
+    add     edi,ebp
+    mov     eax,edi
+    cdq
     idiv    esi
-    mov     [D],eax
-vihod:
-    ret
-    schet   endp   
-Exit:
-    mov     ah,04ch
-    mov     al,0
-    int     21h
-    end     Start 
+    ret     6
+    schet   endp
+end     Start 
